978494adc525e7c0cbeec2eb55493ce17ae16937
Aleksandr <aleksdark13@gmail.com>
2024-05-18 18:41:46 +0300

    add use state

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ function App() {
     const [itemEdit, setItemEdit] = useState(null); // Додали стан для зберігання стану" +
     const [loading, setLoading] = useState(true); // Додали стан для зберігання стану завантаження
 
-    const [formData, setFormData] = useState({
+    const [princes, setPrinces] = useState([]);
+    const [selectedPrince, setSelectedPrince] = useState({
         name: '',
+        id: ''
+    });
+
+    const [formData, setFormData] = useState({
+        prince_id: '',
         lastname: '',
         age: '',
         id: ''
@@ -16,9 +22,12 @@ function App() {
 
     const handleChange = (e) => {
         const {name, value} = e.target;
+
+        const newValue = name === 'age' || name === 'prince_id' ? Number(value) : value;
+
         setFormData(prevState => ({
             ...prevState,
-            [name]: value
+            [name]: newValue
         }));
     };
 
@@ -30,21 +39,32 @@ function App() {
 
     const handleChangeEditableItem = (e) => {
         const {name, value} = e.target;
+        const newValue = name === 'age' || name === 'prince_id' ? parseInt(value, 10) : value;
         setEditableItemData(prevState => ({
             ...prevState,
-            [name]: value
+            [name]: newValue
         }));
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // setFormData(prevState => {
+        //     const selectedPrince = princes.find(prince => prince.id === formData.prince_id);
+        //     return {
+        //         ...prevState,
+        //         name: selectedPrince ? selectedPrince.name : '' // Якщо обраний князь існує, встановіть його ім'я, інакше встановіть порожню рядок
+        //     };
+        // });
+
         try {
+
+
             await axios.post('http://localhost:5000/save_data', formData);
             console.log('Data saved successfully!');
             // Очищення форми після успішного збереження
             setFormData({
-                name: '',
                 lastname: '',
+                prince_id: '',
                 age: '',
                 id: ''
             });
@@ -57,11 +77,25 @@ function App() {
 
     useEffect(() => {
 
+
+        fetchData();
+
         console.log(data);
-        if (data.length === 0) {
-            fetchData();
-        }
-    }, [data]);
+
+    }, []);
+
+    useEffect(() => {
+
+
+        fetch('http://localhost:5000/api/kyivan_princes')
+            .then(response => response.json())
+            .then(data => setPrinces(data))
+            .catch(error => console.error('Error fetching data:', error));
+
+        console.log("princes", princes);
+
+
+    }, []);
 
     const fetchData = async () => {
         try {
@@ -96,14 +130,15 @@ function App() {
 
         axios.post('http://localhost:5000/save_item', {
             id: id,
-            name: editableItemData.name,
+            name: selectedPrince.name,
+            prince_id: selectedPrince.id,
             lastname: editableItemData.lastname,
             age: editableItemData.age
         })
             .then(function (response) {
                 console.log(response.data); // Виводимо повідомлення про успішне оновлення в консоль
-
-                 fetchData();
+                setData([])
+                fetchData();
             })
             .catch(function (error) {
                 console.error('Error:', error);
@@ -114,22 +149,39 @@ function App() {
                 lastname: '',
                 age: '',
             })
+            setSelectedPrince('')
         });
 
     }
+
+
+    const handleChangePrince = (event) => {
+        setSelectedPrince({
+            id: event.target.value,
+            name: princes.find(prince => prince.id === selectedPrince.id),
+        });
+    };
 
     return (
         <div>
             <h1>Data from Flask:</h1>
             <ul>
-                {data.map(item => (
+                {data.length > 0 && data.map(item => (
                     <li key={item.name}>
                         {itemEdit !== item.id ? <p> {item.name} {item.lastname} - {item.age} - {item.id}</p>
                             :
                             <div>
+                                <h1>Список Київських князів</h1>
+                                <select value={selectedPrince} onChange={handleChangePrince}>
+                                    <option value="">Оберіть князя</option>
+                                    {princes.map((prince, index) => (
+                                        <option key={prince.id} value={prince.name}>{prince.name}</option>
+                                    ))}
+                                </select>
                                 <label>Name:</label>
                                 <input type="text" name="name" value={editableItemData.name}
                                        onChange={handleChangeEditableItem}/>
+                                <br/>
                                 <br/>
                                 <label>Lastname:</label>
                                 <input type="text" name="lastname" value={editableItemData.lastname}
@@ -146,6 +198,7 @@ function App() {
 
                         <button disabled={itemEdit !== null} onClick={() => {
                             setItemEdit(item.id)
+                            setSelectedPrince({name: item.name, id: item.prince_id})
                             setEditableItemData({age: item.age, lastname: item.lastname, name: item.name})
                         }}>edit
                         </button>
@@ -157,8 +210,14 @@ function App() {
             <div>
                 <h1>Enter Data:</h1>
                 <form onSubmit={handleSubmit}>
+                    {selectedPrince.name && <p>Ви обрали: {selectedPrince.name}</p>}
                     <label>Name:</label>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+                    <select name="prince_id" value={selectedPrince} onChange={handleChange}>
+                        <option value="">Оберіть князя</option>
+                        {princes.map((prince, index) => (
+                            <option key={prince.id} value={prince.id}>{prince.name}</option>
+                        ))}
+                    </select>
                     <br/>
                     <label>Lastname:</label>
                     <input type="text" name="lastname" value={formData.lastname} onChange={handleChange}/>
@@ -166,7 +225,7 @@ function App() {
                     <label>Age:</label>
                     <input type="number" name="age" value={formData.age} onChange={handleChange}/>
                     <br/>
-                    <button type="submit">Save</button>
+                    <button onClick={handleSubmit} >Save</button>
                 </form>
             </div>
         </div>
